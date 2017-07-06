4706bd8fd88b3cccc105cee877b3149af427fb18
hzlixiutang <hzlixiutang@corp.netease.com>
2017-07-06 18:12:16 +0800

    orgin

--- linux-2.6.11/include/linux/netfilter_ipv4/ipt_mark.h
+++ linux-2.6.11/include/linux/netfilter_ipv4/ipt_mark.h
@@ -1,9 +1,20 @@
-#ifndef _IPT_MARK_H
-#define _IPT_MARK_H
+#ifndef _IPT_MARK_H_target
+#define _IPT_MARK_H_target
 
-struct ipt_mark_info {
-    unsigned long mark, mask;
-    u_int8_t invert;
+/* Version 0 */
+struct ipt_mark_target_info {
+	unsigned long mark;
 };
 
-#endif /*_IPT_MARK_H*/
+/* Version 1 */
+enum {
+	IPT_MARK_SET=0,
+	IPT_MARK_AND,
+	IPT_MARK_OR
+};
+
+struct ipt_mark_target_info_v1 {
+	unsigned long mark;
+	u_int8_t mode;
+};
+#endif /*_IPT_MARK_H_target*/
